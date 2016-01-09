7a6ea2b7b06165e888fba9878c6fd61edb23d29b
Rafael Xavier de Souza <rxaviers@gmail.com>
2016-01-09 16:56:28 -0200

    Make dynamic compiler orphan
    It can still be used, but not part of the regular API.
    
    Ref #7

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var compileDynamically = require("./lib/dynamic-compiler");
 var extract = require( "./lib/extract" );
 var extractDefaultMessages = require( "./lib/extract-default-messages" );
 var generateTranslation = require("./lib/generate-translation");
 var initOrUpdateTranslation = require("./lib/init-or-update-translation");
 
 module.exports = {
-  compileDynamically: compileDynamically,
   extract: extract,
   extractDefaultMessages: extractDefaultMessages,
   generateTranslation: generateTranslation,
